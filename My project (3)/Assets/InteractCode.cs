19dbde1ef26b6e2123e60025cdfa4d9bd12f5d50
InchI2426 <nachiwat2426@gmail.com>
2025-05-13 15:44:42 +0700

    day 3 (2)

---
+++ My project (3)/Assets/InteractCode.cs
@@ -0,0 +1,22 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InteractCode : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+}
